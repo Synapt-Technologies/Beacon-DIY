8e0ccf1d3a3f5ac74e808e8265b8834bd5bee5bb
IJssel <ijssel@synapt.nl>
2026-05-07 13:28:28 +0200

    fixed section led numbers

--- satelites/CYD/CYD-Satelite/main/main.cpp
+++ satelites/CYD/CYD-Satelite/main/main.cpp
@@ -88,7 +88,7 @@ extern "C" void app_main()
     static StripSection ws2812Sections[] = {
         { 0, 1, DeviceAlertTarget::ALL },
         { 16, 0, DeviceAlertTarget::ALL },
-        { 54, 2, DeviceAlertTarget::ALL },
+        { 48, 2, DeviceAlertTarget::ALL },
     };
     IConsumer* consumer1 = new WS2812Consumer(createLedStrip(), ADD_LED_STRIP_LED_NUMBER, ws2812Sections, 3);
     IConsumer* consumer2 = new SimpleRGBConsumer(FIX_LED_R_GPIO, FIX_LED_G_GPIO, FIX_LED_B_GPIO, DeviceAlertTarget::OPERATOR);
